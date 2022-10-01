3af22395e828a66e92c9a1557d23eca9da3a433a
nirmalshah20519 <nirmalshah20519@gmail.com>
2022-10-01 19:42:59 +0530

    Update in Done,js

--- src/components/Done.js
+++ src/components/Done.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 
 const Done = (props) => {
@@ -11,18 +11,22 @@ const Done = (props) => {
     const url="http://testsvojsapi.navrssolutions.co.in/api/account/NewMemberRegistration"
     const url2='https://mockend.com/nirmalshah20519/Mockend/posts'
         
-    
+    const [message, setMessage]=useState('')
     const postToApi= async (e)=>{
       const data=props.finalData
       console.log(data)
-      await  Axios.post(url2, data)
-        .then(res=>{console.log(res.data)})
+      await  Axios.post(url, data)
+        .then(res=>{
+          console.log(res.data)
+          setMessage(res.data.msg)})
+        .catch((e)=>{console.log(e)
+          setMessage(e.message)})
 
     }
 
   return (
     <>
-    <h1> ``` Response Message ``` </h1>
+    <h1> {message} </h1>
     </>
   )
 }
